ad2f0a22eff1253ab9b7589671715c10f91df0b2
Salette <A01246619@tec.mx>
2023-06-05 19:49:30 -0600

    Acomodo de última versión

--- app/controllers/recover_vitals.controller.js
+++ app/controllers/recover_vitals.controller.js
@@ -21,14 +21,15 @@ exports.recover_user = async (req, res) => {
   const contact = await Client.findOne({ where: 
                                         { clientesPruebaIdCliente: userid,  
                                           rolesPruebaRoleid: 4 }});
-
-  const formato = 'hex';   
+  
   saltClient = client.salt;  
-  saltContact = client.salt;      
+  saltContact = client.salt;     
+
   pwdCliente = bcrypt.hashSync(req.body.pwdCliente, saltClient);
   pwdContacto = bcrypt.hashSync(req.body.pwdContacto, saltContact);
   preguntaCliente = bcrypt.hashSync(req.body.preguntaCliente, saltClient);
   preguntaContacto = bcrypt.hashSync(req.body.preguntaContacto, saltContact);
+
   saltPrivadaCliente = client.saltPrivada ;
   saltPrivadaContacto = contact.saltPrivada;
   ivCliente = client.ivUsuario;
@@ -42,33 +43,41 @@ exports.recover_user = async (req, res) => {
   ivPreguntaClienteCipher = client.ivPregunta;
   ivPreguntaContactoCipher = contact.ivPregunta;
 
-  old_keyPrivada_client = client.keyPrivada;
-  old_keyPrivada_contact = contact.keyPrivada;
   old_zc_client = client.zc;
-  old_zc_contact = contact.zc;
   old_zcPwd_client = client.zcPwd;
-  old_zcPwd_contact = contact.zcPwd;
   old_derivedKeyPwd_client = client.derivedKeyPwd;
-  old_derivedKeyPwd_contact = contact.derivedKeyPwd;
 
-  new_credentials = cryp.update_credentials(pwdCliente, pwdContacto, preguntaCliente, preguntaContacto, saltPrivadaCliente, saltPrivadaContacto, ivCliente, ivContacto, saltPwdCliente, saltPwdContacto, ivPwdClienteCipher,  ivPwdContactoCipher, saltPreguntaCliente, saltPreguntaContacto, ivPreguntaClienteCipher,  ivPreguntaContactoCipher);
+  new_credentials = cryp.update_credentials(pwdCliente, pwdContacto, 
+                                          preguntaCliente, preguntaContacto, 
+                                          saltPrivadaCliente, saltPrivadaContacto, 
+                                          ivCliente, ivContacto, 
+                                          saltPwdCliente, saltPwdContacto, 
+                                          ivPwdClienteCipher, ivPwdContactoCipher, 
+                                          saltPreguntaCliente, saltPreguntaContacto, 
+                                          ivPreguntaClienteCipher, ivPreguntaContactoCipher);
 
   new_keyPrivada_client = new_credentials.client_cryp[0];
-  new_keyPrivada_contact = new_credentials.contact_cryp[0];
   new_zc_client = new_credentials.client_cryp[1];
-  new_zc_contact = new_credentials.contact_cryp[1];
   new_derivedKeyPwd_client = new_credentials.client_cryp[2];
-  new_derivedKeyPwd_contact = new_credentials.contact_cryp[2];
   new_zcPwd_client = new_credentials.client_cryp[3];
-  new_zcPwd_contact = new_credentials.contact_cryp[3];
   new_derivedKeyPregunta_client = new_credentials.client_cryp[4];
-  new_derivedKeyPregunta_contact = new_credentials.contact_cryp[4];
   new_zcPregunta_client = new_credentials.client_cryp[5];
+  
+  new_keyPrivada_contact = new_credentials.contact_cryp[0];
+  new_zc_contact = new_credentials.contact_cryp[1];
+  new_derivedKeyPwd_contact = new_credentials.contact_cryp[2];
+  new_zcPwd_contact = new_credentials.contact_cryp[3];
+  new_derivedKeyPregunta_contact = new_credentials.contact_cryp[4];
   new_zcPregunta_contact = new_credentials.contact_cryp[5];
 
 
-  const Z1_inverso = cryp.get_sharedSecret(old_zc_client, old_zcPwd_client, old_derivedKeyPwd_client, ivPwdClienteCipher, saltPrivadaCliente, ivCliente);
-  const new_Z1_inverso = cryp.get_sharedSecret(new_zc_client, new_zcPwd_client, new_derivedKeyPwd_client, ivPwdClienteCipher, saltPrivadaCliente, ivCliente);
+  const Z1_inverso = cryp.get_sharedSecret(old_zc_client, old_zcPwd_client, 
+                                          old_derivedKeyPwd_client, ivPwdClienteCipher, 
+                                          saltPrivadaCliente, ivCliente);
+
+  const new_Z1_inverso = cryp.get_sharedSecret(new_zc_client, new_zcPwd_client, 
+                                              new_derivedKeyPwd_client, ivPwdClienteCipher, 
+                                              saltPrivadaCliente, ivCliente);
 
   for(const objeto in old_vitals){
 
